5e06f881a93a8daf8de417c32313cf54c1555444
임선용 <dicebattle@gmail.com>
2012-07-10 17:42:05 +0900

    test

--- codebone-template/src/main/java/org/codebone/security/authorities/AuthoritiesModel.java
+++ codebone-template/src/main/java/org/codebone/security/authorities/AuthoritiesModel.java
@@ -1,12 +1,16 @@
 package org.codebone.security.authorities;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
 import org.codebone.framework.generic.AbstractModel;
+import org.codebone.security.group.GroupModel;
 
 
 @Entity
@@ -22,47 +26,4 @@ public class AuthoritiesModel extends AbstractModel{
 	
 	@Column
 	private String authority;
-
-	public Long getIdx() {
-		return idx;
-	}
-
-	public void setIdx(Long idx) {
-		this.idx = idx;
-	}
-
-	public Long getPrinciple() {
-		return principle;
-	}
-
-	public void setPrinciple(Long principle) {
-		this.principle = principle;
-	}
-
-	public String getAuthority() {
-		return authority;
-	}
-
-	public void setAuthority(String authority) {
-		this.authority = authority;
-	}
-
-	@Override
-	public String toString() {
-		return "AuthoritiesModel [idx=" + idx + ", principle=" + principle
-				+ ", authority=" + authority + "]";
-	}
-
-	public AuthoritiesModel(Long idx, Long principle, String authority) {
-		super();
-		this.idx = idx;
-		this.principle = principle;
-		this.authority = authority;
-	}
-
-	public AuthoritiesModel() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-	
 }
