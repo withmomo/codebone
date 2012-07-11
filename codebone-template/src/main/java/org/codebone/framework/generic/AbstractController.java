c27c21c2a74e1a1db2ca7fe0e3bfaa475efde351
임선용 <dicebattle@gmail.com>
2012-07-11 16:54:33 +0900

    merge test2

---
+++ codebone-template/src/main/java/org/codebone/framework/generic/AbstractController.java
@@ -0,0 +1,127 @@
+package org.codebone.framework.generic;
+
+import java.text.ParseException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.codebone.security.manager.ManagerModel;
+import org.codebone.security.manager.ManagerService;
+import org.codebone.security.menu.MenuModel;
+import org.codebone.security.menu.MenuService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public abstract class AbstractController<M extends AbstractModel> {
+
+	@Autowired
+	protected ManagerService managerService;
+
+	@Autowired
+	protected MenuService menuService;
+	
+	protected abstract String getContextName();
+	
+	protected abstract AbstractService getService();
+
+	public ModelAndView getCommonModelAndView(String target,
+			Map<String, Object> map) {
+		Authentication auth = SecurityContextHolder.getContext()
+				.getAuthentication();
+		ManagerModel currentLoginManager = (ManagerModel) managerService.read(
+				Long.parseLong(auth.getName())).getData();
+		List<MenuModel> list = (List<MenuModel>) menuService.listAll()
+				.getData();
+		System.out.println(list);
+		map.put("loginManager", currentLoginManager);
+		map.put("menu", list);
+		return new ModelAndView(target, map);
+	}
+
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public ModelAndView list(HttpServletRequest req, HttpServletResponse res,
+			HttpSession session, Integer page) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		if (page == null) {
+			page = 1;
+		}
+		map.put("data", getService().list(page));
+		map.put("page", page);
+
+		return getCommonModelAndView(getContextName()+"/list", map);
+	}
+
+	@RequestMapping(value = "/search", method = RequestMethod.POST)
+	public ModelAndView search(HttpServletRequest req, HttpServletResponse res,
+			HttpSession session, Integer page) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		String property = req.getParameter("property");
+		String keyword = req.getParameter("keyword");
+		if (page == null) {
+			page = 0;
+		}
+		map.put("data", getService().search(property, keyword, page));
+		map.put("page", page);
+		return getCommonModelAndView(getContextName()+"/list", map);
+	}
+
+	@RequestMapping(value = "/create", method = RequestMethod.GET)
+	public ModelAndView create(HttpServletRequest req, HttpServletResponse res,
+			HttpSession session) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("isCreate", "Y");
+		return getCommonModelAndView(getContextName()+"/update", map);
+	}
+
+	@RequestMapping(value = "/create", method = RequestMethod.POST)
+	public ModelAndView create_POST(HttpServletRequest req,
+			HttpServletResponse res, HttpSession session,
+			@ModelAttribute M model) throws ParseException {
+		getService().create(model);
+		/**
+		 * Create Complete
+		 */
+		return list(req, res, session, 0);
+	}
+
+	@RequestMapping(value = "/update", method = RequestMethod.GET)
+	public ModelAndView update(HttpServletRequest req, HttpServletResponse res,
+			HttpSession session, String idx) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("data", getService().read(idx));
+		map.put("id", idx);
+		map.put("isCreate", "N");
+		return getCommonModelAndView(getContextName()+"/update", map);
+	}
+
+	@RequestMapping(value = "/update", method = RequestMethod.POST)
+	public ModelAndView update_POST(HttpServletRequest req,
+			HttpServletResponse res, HttpSession session,
+			@ModelAttribute M managerModel) throws ParseException {
+		getService().update(managerModel);
+		/**
+		 * Update Complete
+		 */
+		return list(req, res, session, 0);
+	}
+
+	@RequestMapping(value = "/delete", method = RequestMethod.GET)
+	public ModelAndView delete(HttpServletRequest req, HttpServletResponse res,
+			HttpSession session, String idx) {
+		M model = (M) getService().read(idx).getData();
+		getService().delete(model);
+
+		return list(req, res, session, 0);
+	}
+}
---
+++ codebone-template/src/main/webapp/WEB-INF/jsp/menu/update.jsp
@@ -0,0 +1,107 @@
+<%@page import="java.text.SimpleDateFormat"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%@ page
+	import="org.codebone.framework.BaseModel,java.util.List,org.codebone.security.menu.MenuModel,org.codebone.security.manager.ManagerModel"%>
+<%@ page
+	import="java.lang.reflect.Field,java.lang.reflect.Method,javax.persistence.Id,java.util.Date"%>
+<%
+	String isCreate = (String) request.getAttribute("isCreate");
+	ManagerModel currentLoginManager = (ManagerModel) request
+			.getAttribute("loginManager");
+	MenuModel menuModel = null;
+	String key = null;
+	if (isCreate.equals("Y")) {
+		menuModel = new MenuModel();
+		isCreate = "Create";
+	} else {
+		BaseModel model = (BaseModel) request.getAttribute("data");
+		key = (String) request.getAttribute("id");
+		menuModel = (MenuModel) model.getData();
+		isCreate = "Update";
+	}
+%>
+<!DOCTYPE HTML>
+<html>
+<%@ include file="/WEB-INF/jsp/import/config.jsp"%>
+<link href="../../../css/datepicker.css" rel="stylesheet">
+<script type="text/javascript" src="../../../js/bootstrap-datepicker.js"></script>
+<title><%=MenuModel.class.getSimpleName() + " " + isCreate%></title>
+<body>
+	<form class="well form-search"
+		action="<%=request.getContextPath()%>/admin/menu/<%=isCreate.toLowerCase()%>"
+		method="post">
+		<table class="table table-striped">
+			<thead>
+				<tr>
+					<th>필드</th>
+					<th>값</th>
+				</tr>
+			</thead>
+
+
+			<tr>
+				<th>name</th>
+				<td><input type="text" class="form-vertical" name="name"
+					value="<%=menuModel.getName()%>"></td>
+			</tr>
+			<tr>
+				<th>url</th>
+				<td><input type="text" class="form-vertical" name="url"
+					value="<%=menuModel.getUrl()%>"></td>
+			</tr>
+			<tr>
+				<th>priOrder</th>
+				<td><input type="text" class="form-vertical" name="priOrder"
+					value="<%=menuModel.getPriOrder()%>"></td>
+			</tr>
+			<tr>
+				<th>subOrder</th>
+				<td><input type="text" class="form-vertical" name="subOrder"
+					value="<%=menuModel.getSubOrder()%>"></td>
+			</tr>
+			<tr>
+				<th>isSeparate</th>
+				<td><input type="text" class="form-vertical" name="isSeparate"
+					value="<%=menuModel.getIsSeparate()%>"></td>
+			</tr>
+			<tr>
+				<th>isExternal</th>
+				<td><input type="text" class="form-vertical" name="isExternal"
+					value="<%=menuModel.getIsExternal()%>"></td>
+			</tr>
+			<tr>
+				<th>managerIdx</th>
+				<td><input type="text" class="form-vertical" name="managerIdx"
+					<%if (isCreate.equals("Create")) {%>
+					value="<%=currentLoginManager.getIdx()%>"></td>
+				<%
+					} else {
+				%>
+				value="<%=menuModel.getManagerIdx()%>">
+				</td>
+				<%
+					}
+				%>
+			</tr>
+			</tbody>
+		</table>
+		<div style="text-align: right">
+			<button class="btn btn-primary btn-large" type="submit">
+				<i class="icon-pencil icon-white"></i><%=isCreate%>
+			</button>
+		</div>
+		<%
+			if (isCreate.equals("Update")) {
+		%>
+		<input type="hidden" class="form-vertical" name="idx"
+			value="<%=menuModel.getIdx()%>">
+		</td>
+		<%
+			}
+		%>
+	</form>
+</body>
+</html>
