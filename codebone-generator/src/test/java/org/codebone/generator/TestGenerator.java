4f42119fbcafc12f55faa462a1aba1decafc7812
임선용 <dicebattle@gmail.com>
2012-07-28 16:44:14 +0900

    테스트 코드가 여러개의 테이블을 한번에 생성하도록 코드 수정

--- codebone-generator/src/test/java/org/codebone/generator/TestGenerator.java
+++ codebone-generator/src/test/java/org/codebone/generator/TestGenerator.java
@@ -13,36 +13,45 @@ import org.codebone.generator.connector.TableHelper;
 import org.junit.Test;
 
 public class TestGenerator {
-	
+
 	private List<Column> columns;
-	
+
 	@Test
 	public void generate() throws SQLException {
-		String teamplatePath = "template/";
-		String generatePath = "test/";
-		String tableName = "user";
-		String packageName = "kr.io";
-		String uri = "test";
-		String siteTitle = "title";
-		loadData(tableName);
-		
-		Generator generator = new Generator();
-		generator.setSiteTitle(siteTitle);
-		generator.setTeamplatePath(teamplatePath);
-		generator.setGeneratePath(generatePath);
-		generator.setColumns(columns);
-		generator.setTableName(tableName);
-		generator.setPackageName(packageName);
-		generator.setUri(uri);
-		generator.generate();
+		String tableNames[] = { "CrashLog", "Item", "ItemStatistics",
+				"Message", "MessageComments", "MyItem", "Notice",
+				"NotifyMessage", "PlaneExpectRoute", "PlaneInformation",
+				"PointLog", "PointStatistics", "PostBox", "User" };
+		for(String tableName : tableNames){
+			String teamplatePath = "template/";
+			String generatePath = "test/";
+			String packageName = "org.codebone.domain." + tableName.toLowerCase();
+			String uri = tableName.toLowerCase();
+			String siteTitle = tableName;
+			loadData(tableName);
+
+			Generator generator = new Generator();
+			generator.setSiteTitle(siteTitle);
+			generator.setDatabaseType(DatabaseType.MYSQL);
+			generator.setTeamplatePath(teamplatePath);
+			generator.setGeneratePath(generatePath);
+			generator.setColumns(columns);
+			generator.setTableName(tableName);
+			generator.setPackageName(packageName);
+			generator.setUri(uri);
+			generator.generate();
+		}
 	}
-	
+
 	public void loadData(String tableName) throws SQLException {
-		DatabaseConfiguration databaseConfiguration = new DatabaseConfiguration(DatabaseType.MYSQL, Define.host, Define.port, Define.database, Define.id, Define.password);
+		DatabaseConfiguration databaseConfiguration = new DatabaseConfiguration(
+				DatabaseType.MYSQL, Define.host, Define.port, Define.database,
+				Define.id, Define.password);
 		Connection connection = null;
-		DatabaseConnector databaseConnector = new MySQLDatabaseConnector(databaseConfiguration);
+		DatabaseConnector databaseConnector = new MySQLDatabaseConnector(
+				databaseConfiguration);
 		connection = databaseConnector.getConnection();
-		columns = TableHelper.getColumns(databaseConnector,tableName);
+		columns = TableHelper.getColumns(databaseConnector, tableName);
 		connection.close();
 	}
 }
