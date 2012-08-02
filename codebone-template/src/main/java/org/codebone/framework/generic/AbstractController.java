b87c2cb41bdc9bc473121dbcbb4d4995a52997ed
임선용 <dicebattle@gmail.com>
2012-08-02 22:28:24 +0900

    #41 yyyy-MM-dd HH:mm:ss.0 패턴에 대한 바인더 처리 완료
    yyyy-MM-dd 로 날짜까지만 작성할 경우 바인딩 되지 않는 문제가 있음.

--- codebone-template/src/main/java/org/codebone/framework/generic/AbstractController.java
+++ codebone-template/src/main/java/org/codebone/framework/generic/AbstractController.java
@@ -1,6 +1,8 @@
 package org.codebone.framework.generic;
 
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,10 +16,13 @@ import org.codebone.security.manager.ManagerService;
 import org.codebone.security.menu.Menu;
 import org.codebone.security.menu.MenuService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -51,5 +56,11 @@ public abstract class AbstractController<M extends AbstractModel> {
 		map.put("loginManager", currentLoginManager);
 		map.put("menu", menuList);
 		return new ModelAndView(target, map);
+	}
+	
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+	    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.0");
+	    binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));
 	}
 }
