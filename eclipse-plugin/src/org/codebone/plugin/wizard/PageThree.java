d5dae7d0bbc89177364567a2428a0ab124315b06
jykimm251 <jykimm251@gmail.com>
2012-10-25 00:31:14 +0900

    플러그인 업데이트

--- eclipse-plugin/src/org/codebone/plugin/wizard/PageThree.java
+++ eclipse-plugin/src/org/codebone/plugin/wizard/PageThree.java
@@ -1,5 +1,6 @@
 package org.codebone.plugin.wizard;
 
+import java.sql.Types;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,108 +14,127 @@ import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 
 public class PageThree extends WizardPage {
+
+	final static String COLUMN_NAME = "columnName";
+	final static String COLUMN_DESCRIPCION = "columnDescription";
+	final static String COLUMN_TYPE = "columnType";
+	final static String COLUMN_SEARCHABLE = "columnSearchable";
+
+	private Composite container;
+
+	private List<Column> columns = null;
+	private List<Text> descriptions = null;
+	private List<Button> searchables = null;
+	private List<Button> writeables = null;
+
+	public PageThree() {
+		super("Columns");
+		setTitle("Columns");
+		setDescription("Fill columns. If you see 'selectbox', column description reduce your code."
+				+ "When column description is 'operation mode{0=development,1=staging}',wizard replace 'selectbox'."
+				+ "" + "Column description : mode{0=development, 1=staging}");
+	}
+
+	@Override
+	public void createControl(Composite parent) {
+		container = new Composite(parent, SWT.NULL);
+		GridLayout layout = new GridLayout();
+		container.setLayout(layout);
+		layout.numColumns = 6;
+
+		Label label0 = new Label(container, SWT.NULL);
+		label0.setText("");
+
+		Label label1 = new Label(container, SWT.NULL);
+		label1.setText("Name");
+
+		Label label2 = new Label(container, SWT.NULL);
+		label2.setText("Description");
+
+		Label label3 = new Label(container, SWT.NULL);
+		label3.setText("Type");
+
+		Label label4 = new Label(container, SWT.NULL);
+		label4.setText("Writeable");
+
+		Label label5 = new Label(container, SWT.NULL);
+		label5.setText("Searchable");
+
+		// check.setSelection(true);
+		// Required to avoid an error in the system
+		setControl(container);
+		setPageComplete(false);
+
+		setPageComplete(true);
+	}
+
+	public void draw() {
+		descriptions = new ArrayList<Text>();
+		searchables = new ArrayList<Button>();
+		writeables = new ArrayList<Button>();
+
+		if (columns != null) {
+			for (int i = 0; i < columns.size(); i++) {
+				Column column = columns.get(i);
+
+				// loop
+				Label label_t1 = new Label(container, SWT.NULL);
+				label_t1.setText("" + (i + 1));
+
+				Label label_t2 = new Label(container, SWT.NULL);
+				label_t2.setText(column.getName());
+
+				Text text_t1 = new Text(container, SWT.SINGLE);
+				text_t1.setText(column.getName());
+				descriptions.add(text_t1);
+
+				Label label_t3 = new Label(container, SWT.NULL);
+				label_t3.setText(column.getTypeName());
+
+				Button writeableCheckBox = new Button(container, SWT.CHECK);
+				boolean isPredefinedColumn = isPredefinedColumn(column);
+				if( isPredefinedColumn )
+					writeableCheckBox.setSelection(false);
+				else
+					writeableCheckBox.setSelection(true);
+				
+				writeables.add(writeableCheckBox);
+				
+				Button searchableCheckBox = new Button(container, SWT.CHECK);
+				searchables.add(searchableCheckBox);
+			}
+		}
+
+		container.layout();
+	}
 	
-	final static String COLUMN_NAME 		= "columnName";
-	final static String COLUMN_DESCRIPCION 	= "columnDescription";
-	final static String COLUMN_TYPE 		= "columnType";
-	final static String COLUMN_SEARCHABLE 	= "columnSearchable";
-	
-	  private Composite container;
+	public boolean isPredefinedColumn(Column column) {
+		if( column.isPrimaryKey() )
+			return true;
+		else if( column.getType() == Types.TIMESTAMP )
+			return true;
+		
+		return false;
+	}
 
-	  private List<Column> columns     = null;
-	  private List<Text>   inputText   = null;
-	  private List<Button> inputButton = null;
-	  
-	  public PageThree() {
-	    super("Columns");
-	    setTitle("Columns");
-	    setDescription("Fill columns. If you see 'selectbox', column description reduce your code." +
-	    		"When column description is 'operation mode{0=development,1=staging}',wizard replace 'selectbox'." +
-	    		"" +
-	    		"Column description : mode{0=development, 1=staging}");
-	  }
-
-	  @Override
-	  public void createControl(Composite parent) {
-	    container = new Composite(parent, SWT.NULL);
-	    GridLayout layout = new GridLayout();
-	    container.setLayout(layout);
-	    layout.numColumns = 5;
-	    
-	    Label label0 = new Label(container, SWT.NULL);
-	    label0.setText("");
-	    
-	    Label label1 = new Label(container, SWT.NULL);
-	    label1.setText("Name");
-	    
-	    Label label2 = new Label(container, SWT.NULL);
-	    label2.setText("Description");
-	    
-	    Label label3 = new Label(container, SWT.NULL);
-	    label3.setText("Type");
-	    
-	    Label label4 = new Label(container, SWT.NULL);
-	    label4.setText("Searchable");
-	    
-	    //check.setSelection(true);
-	    // Required to avoid an error in the system
-	    setControl(container);
-	    setPageComplete(false);
-	    
-	    setPageComplete(true);
-	  }
-	  
-	  public void draw() {
-		  inputText = new ArrayList<Text>();
-		  inputButton = new ArrayList<Button>();
-		  
-		    if (columns != null) {
-		    	
-		    	for (int i=0;i<columns.size();i++) {
-		    		Column column = columns.get(i);
-		    		
-				    //loop
-				    Label label_t1 = new Label(container, SWT.NULL);
-				    label_t1.setText(""+(i+1));
-				    
-				    Label label_t2 = new Label(container, SWT.NULL);
-				    label_t2.setText(column.getName());
-				    
-				    Text text_t1 = new Text(container, SWT.SINGLE);
-				    text_t1.setText(column.getName());
-				    inputText.add(text_t1);
-				    
-				    Label label_t3 = new Label(container, SWT.NULL);
-				    label_t3.setText(column.getTypeName());
-				    
-				    Button check = new Button(container, SWT.CHECK);
-				    inputButton.add(check);
-		    	}
-		    }
-		    
-		    container.layout();
-	  }
-
-	/**
-	 * @return the columns
-	 */
 	public List<Column> getColumns() {
 		return columns;
 	}
-
-	/**
-	 * @param columns the columns to set
-	 */
+	
 	public void setColumns(List<Column> columns) {
 		this.columns = columns;
 	}
-	  
+
 	public String getDescription(int idx) {
-		return inputText.get(idx).getText();
+		return descriptions.get(idx).getText();
 	}
-	
+
 	public boolean getSearchable(int idx) {
-		return inputButton.get(idx).getSelection();
+		return searchables.get(idx).getSelection();
 	}
-	  
+
+	public boolean getWriteables(int idx) {
+		return writeables.get(idx).getSelection();
+	}
+
 }
