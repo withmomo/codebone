d46f7a662128f3365859612ee15fe755f79c6460
Sunyong Lim <dicebattle@gmail.com>
2013-01-12 03:46:43 +0900

    #11 change argument find relationship with foreign key

--- generator/src/main/java/org/codebone/console/Codebone.java
+++ generator/src/main/java/org/codebone/console/Codebone.java
@@ -1,9 +1,29 @@
 package org.codebone.console;
 
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.cli.Options;
+import org.codebone.connector.DatabaseConfiguration;
+import org.codebone.connector.DatabaseConnector;
+import org.codebone.connector.DatabaseHelper;
+import org.codebone.connector.DatabaseType;
+import org.codebone.connector.MySQLDatabaseConnector;
+
+import schemacrawler.schema.Column;
+import schemacrawler.schema.Database;
+import schemacrawler.schema.Index;
+import schemacrawler.schema.IndexColumn;
+import schemacrawler.schema.PrimaryKey;
+import schemacrawler.schema.Schema;
+import schemacrawler.schema.Table;
+import schemacrawler.schemacrawler.SchemaCrawlerOptions;
+import schemacrawler.schemacrawler.SchemaInfoLevel;
+import schemacrawler.utility.SchemaCrawlerUtility;
 
 public class Codebone extends BaseCommand {
 	
@@ -12,51 +32,135 @@ public class Codebone extends BaseCommand {
 	private String user;
 	private String password;
 	private String table;
+	private String port;
+	private String databaseType;
 	
 	@Override
 	public void run(CommandLine line) throws Exception {
 	    setValues(line);
+	    DatabaseType dbType = null;
+	    databaseType = databaseType.toUpperCase();
+	    System.out.println("database type : " + databaseType);
+	    System.out.println("host : " + host);
+	    System.out.println("user : " + user);
+	    System.out.println("password : " + password);
+	    System.out.println("port : " + port);
+	    System.out.println("database : " + database);
+	    System.out.println("table : " + table);
+	    if(databaseType.equals(DatabaseType.MYSQL.toString())){
+	    	dbType = DatabaseType.MYSQL;
+	    }else if(databaseType.equals(DatabaseType.MSSQL.toString())){
+	    	dbType = DatabaseType.MSSQL;
+	    }
+	    DatabaseConfiguration dbConf = new DatabaseConfiguration(dbType, host, Integer.parseInt(port), database, user, password);
+	    Connection connection = null;
+	    DatabaseConnector connector = new MySQLDatabaseConnector(dbConf);
+		connection = connector.getConnection();
+	    List<String> tableList = DatabaseHelper.getTables(connector);
+	    if(!tableList.contains(table)){
+	    	System.out.println("Table not found!");
+	    	return;
+	    }
+	    final SchemaCrawlerOptions options = new SchemaCrawlerOptions();
+		options.setSchemaInfoLevel(SchemaInfoLevel.standard());
+
+		final Database databaseStruct = SchemaCrawlerUtility.getDatabase(connection, options);
+		Schema schema = databaseStruct.getSchema(database);
+		final Table tableStruct = databaseStruct.getTable(schema, table);
+			  List<Column> oneToManyColumnList = new ArrayList();
+			  for(Column column : tableStruct.getColumns()){
+				  if(column.isPartOfForeignKey()){
+					  Column referencedColumn = column.getReferencedColumn();
+					  if(isUnique(column)){
+						  System.out.println("OneToOne Detected!");
+						  System.out.println(referencedColumn.getParent().getName()+ " 1 -> 1 " + column.getParent().getName());
+					  }else{
+						  System.out.println("OneToMany Detected!");
+						  System.out.println(referencedColumn.getParent().getName()+ " 1 -> N " + column.getParent().getName());
+						  oneToManyColumnList.add(column);
+					  }
+				  }
+				  if(oneToManyColumnList.size()==2 && tableStruct.getColumns().size()==2){
+					  System.out.println("ManyToMany Detected!");
+					  System.out.println(oneToManyColumnList.get(0).getReferencedColumn().getParent().getName() + " N <-> N " 
+					  + oneToManyColumnList.get(1).getReferencedColumn().getParent().getName());
+				  }
+			  }
 	    
-	    // TODO write your code
-	    //
+	    
+	    
+	    
+	}
+	
+	private static boolean isUnique(Column column){
+		for (Index index : column.getParent().getIndices()) {
+            if (index.isUnique()) {
+                List<IndexColumn> indexColumns = index.getColumns();
+                if (indexColumns.size() == 1 && 
+                		indexColumns.get(0).getFullName().equals(column.getFullName())) {
+                	return true;
+                }
+            }
+        }
+		PrimaryKey pk = column.getParent().getPrimaryKey();
+		if(pk != null){
+			List<IndexColumn> PKList = pk.getColumns();
+			if(PKList.size() == 1 &&
+					PKList.get(0).getFullName().equals(column.getFullName())){
+				return true;
+			}
+		}
+		return false;
 	}
 
 	private void setValues(CommandLine line) {
-		host = line.getOptionValue("host");
-	    database = line.getOptionValue("database");
-	    user = line.getOptionValue("user");
-	    password = line.getOptionValue("password");
-	    table = line.getOptionValue("table");
+		host = line.getOptionValue("h");
+	    database = line.getOptionValue("dbname");
+	    user = line.getOptionValue("u");
+	    password = line.getOptionValue("p");
+	    table = line.getOptionValue("t");
+	    port = line.getOptionValue("port");
+	    databaseType = line.getOptionValue("type");
 	}
 	
 	@Override
 	@SuppressWarnings("static-access")
 	public Options options() {
 		Option hostOption = OptionBuilder.withArgName("host").hasArg()
-				.isRequired(true).withDescription("Datababase host")
-				.create("host");
+				.isRequired(true).withDescription("Database host")
+				.create("h");
 
 		Option userOption = OptionBuilder.withArgName("user").hasArg()
 				.isRequired(true).withDescription("Database username")
-				.create("user");
+				.create("u");
 
 		Option passwordOption = OptionBuilder.withArgName("password").hasArg()
 				.isRequired(true).withDescription("Database password")
-				.create("password");
+				.create("p");
 
 		Option databaseOption = OptionBuilder.withArgName("database").hasArg()
 				.isRequired(true).withDescription("Database name")
-				.create("database");
+				.create("dbname");
+		
+		Option databasePort = OptionBuilder.withArgName("port").hasArg()
+				.isRequired(false).withDescription("Database port")
+				.create("port");
+		
+		Option databaseType = OptionBuilder.withArgName("type").hasArg()
+				.isRequired(true).withDescription("Database type")
+				.create("type");
 
-		Option tablaeOption = OptionBuilder.withArgName("table").hasArg()
-				.withDescription("Database table").create("table");
+		Option tableOption = OptionBuilder.withArgName("table").hasArg()
+				.withDescription("Database table").create("t");
 
 		Options options = new Options();
 		options.addOption(hostOption);
 		options.addOption(userOption);
 		options.addOption(passwordOption);
 		options.addOption(databaseOption);
-		options.addOption(tablaeOption);
+		options.addOption(tableOption);
+		options.addOption(databasePort);
+		options.addOption(databaseType);
 
 		return options;
 	}
