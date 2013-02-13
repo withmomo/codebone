f6bc58ec946f39f22d92d9280744c31459642c89
Sung-ju Jin <moleskine7@gmail.com>
2013-02-13 21:46:10 +0900

    fix tests

--- generator/src/test/java/org/codebone/generator/TestFileSanner.java
+++ generator/src/test/java/org/codebone/generator/TestFileSanner.java
@@ -10,7 +10,7 @@ import org.junit.Test;
 public class TestFileSanner {
 	@Test
 	public void scan() throws FileNotFoundException {
-		String path = "src/main/template";
+		String path = "src/main/";
 		new FileScanner(path, new FileListner() {
 			public void found(File file) {
 				System.out.println(file.getPath());
